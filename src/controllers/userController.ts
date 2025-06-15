bf81e1c887a878dd554d0b7d95d7b49f168223c7
Renan Muller <renanmuller@hotmail.com.br>
2025-06-15 20:27:59 -0300

    feat: added login user login controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -69,3 +69,40 @@ export const getUsers = async (req: Request, res: Response) => {
     return res.status(500).json({ message: 'Erro ao buscar usuários' });
   }
 };
+
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Email e senha são obrigatórios' });
+    }
+
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({ message: 'Credenciais inválidas' });
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Credenciais inválidas' });
+    }
+
+    return res.status(200).json({
+      message: 'Login realizado com sucesso',
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+  } catch (error) {}
+};
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
-import { createUser, getUsers } from '../controllers/userController';
+import { createUser, getUsers, loginUser } from '../controllers/userController';
 
 const router = Router();
 
 router.post('/register', createUser);
 router.get('/users', getUsers);
+router.post('/login', loginUser);
 
 export default router;
