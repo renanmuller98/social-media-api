5486d6d5fa738e5b221e4d92d9d5c4fc7ca44554
Renan Muller <renanmuller@hotmail.com.br>
2025-06-15 19:01:43 -0300

    chore: initial project setup with express, typescript and prisma

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+export default app;
