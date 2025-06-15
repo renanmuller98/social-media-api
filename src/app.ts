f5cfff917c4be4f451d17357357fc67fc167bd4d
Renan Muller <renanmuller@hotmail.com.br>
2025-06-15 19:33:56 -0300

    feat: created userController and userRoutes to list and register users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import useRoutes from './routes/userRoutes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use('/', useRoutes);
 
 const PORT = process.env.PORT;
 
---
+++ src/controllers/userController.ts
@@ -0,0 +1,52 @@
+import { Request, Response } from 'express';
+import db from '../prisma';
+
+export const createUser = async (req: Request, res: Response) => {
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!name || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Todos os campos são obrigatórios' });
+    }
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'Usuário já existe' });
+    }
+
+    const user = await db.user.create({
+      data: {
+        name,
+        email,
+        password,
+      },
+      include: {
+        posts: true,
+      },
+    });
+
+    return res
+      .status(201)
+      .json({ message: 'Usuário criado com sucesso', user });
+  } catch (error) {
+    console.error('Erro ao criar usuário:', error);
+    return res.status(500).json({ message: 'Erro ao criar usuário' });
+  }
+};
+
+export const getUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await db.user.findMany();
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error('Erro ao buscar usuários:', error);
+    return res.status(500).json({ message: 'Erro ao buscar usuários' });
+  }
+};
